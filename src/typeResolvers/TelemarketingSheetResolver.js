118c8cf9f1f78cdfe5f535c07c6e89bc526146be
Nicolas Oga <2112.oga@gmail.com>
2020-04-09 20:08:38 -0700

    Fix Telemaarketing sheet resolver bug

--- src/typeResolvers/TelemarketingSheetResolver.js
+++ src/typeResolvers/TelemarketingSheetResolver.js
@@ -3,6 +3,9 @@ import times from 'lodash.times'
 
 import { PhoneNumber, Customer } from '../models'
 
+const collectionContainsNumber = (collection, lastNumbers) =>
+  !!collection.find(pn => pn.number.slice(-2) === lastNumbers)
+
 const TelemarketingSheetResolver = {
   numberInfo: async (sheet, _args, { currentUser }) => {
     const { countryCode, areaCode, firstNumbers } = sheet.dataValues
@@ -29,15 +32,12 @@ const TelemarketingSheetResolver = {
     const teamMembers = team && await team.getMembers({
       where: { id: { [Op.ne]: currentUser.id } }
     })
-    const teamPhoneNumbers = team && await findPhoneNumbers(teamMembers.map(tm => tm.id))
-
-    const collectionContainsNumber = (collection, lastNumbers) =>
-      !!collection.find(pn => pn.number.slice(-2) === lastNumbers)
+    const teamPhoneNumbers = teamMembers && await findPhoneNumbers(teamMembers.map(tm => tm.id))
 
     return times(100, n => {
       const lastNumbers = n.toString().padStart(2, '0')
       const hasContact = collectionContainsNumber(phoneNumbers, lastNumbers)
-      const teamHasContact = collectionContainsNumber(
+      const teamHasContact = !!teamPhoneNumbers && collectionContainsNumber(
         teamPhoneNumbers,
         lastNumbers
       )
